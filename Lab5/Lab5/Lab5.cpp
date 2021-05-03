2ef2ac1fda8ebdb7fa855b69bf2963326f6a53d0
Alex <olexandrpasalsky@gmail.com>
2021-05-03 23:32:45 +0300

    add method "read_code"

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 int main()
 {
-
-    Tree tree;
-    tree.push_operator("+ abc a");
+    string file;
+    getline(cin, file);
+    Reader scan;
+    Tree tree = scan.read_code(file);
+    tree.print_tree();
+    /*tree.push_operator("+ abc a");
     tree.push_operator("= 2 a");
 
     tree.push_operator("+ / ^ ^ 3 2 - 5 1 * 2 4 3");
@@ -17,7 +20,7 @@ int main()
     string s;
     getline(cin, s);
     Reader reader;
-    cout << reader.getPostfix(s);
+    cout << reader.getPostfix(s);*/
     return 0;
 }
 
